e5bb7bef9c35d5dfa3bb63b553144951707ac659
Stefan Micheelsen <sm@intempus.dk>
2024-10-10 22:27:46 +0200

    multiple and styling

--- web/views.py
+++ web/views.py
@@ -8,10 +8,13 @@ import requests
 
 
 def index(request):
-    response = requests.get('http://optimal_power_time_calculator:80/api/next-optimal-hour?numHoursToForecast=3h35m')
-    price = json_to_price("Test", response.json()['price'])
-    print(price)
-    return render(request, 'index.html', {'price': price})
+    response1 = requests.get('http://optimal_power_time_calculator:80/api/next-optimal-hour?numHoursToForecast=3h35m')
+    price1 = json_to_optimal_time_appliance("Test", response1.json()['price'])
+
+    response = requests.get('http://optimal_power_time_calculator:80/api/next-optimal-hour?numHoursToForecast=1h35m')
+    price = json_to_optimal_time_appliance("Test", response.json()['price'])
+
+    return render(request, 'index.html', {'optimal_times': [price, price1]})
 
 
 @dataclass
@@ -22,11 +25,11 @@ class OptimalTimeAppliance:
     price: Decimal
     suboptimal_price_multiplier: Decimal
 
-def json_to_price(name, json_price):
+def json_to_optimal_time_appliance(name, json):
     return OptimalTimeAppliance(
         name=name,
-        from_dt=datetime.fromisoformat(json_price['fromTs']),
-        to_dt=datetime.fromisoformat(json_price['toTs']),
-        price=Decimal(json_price['price']),
-        suboptimal_price_multiplier=Decimal(json_price['suboptimalPriceMultiplier']),
+        from_dt=datetime.fromisoformat(json['fromTs']),
+        to_dt=datetime.fromisoformat(json['toTs']),
+        price=Decimal(json['price']),
+        suboptimal_price_multiplier=Decimal(json['suboptimalPriceMultiplier']),
     )
