916b91609e7ae7cd6b52e4c262dc812d66f43a7a
Stefan Micheelsen <sm@intempus.dk>
2024-10-10 22:14:08 +0200

    Basic site

--- optimal_power_time_calculator/settings.py
+++ optimal_power_time_calculator/settings.py
@@ -70,7 +70,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'optimal_power_time_calculator.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
--- web/views.py
+++ web/views.py
@@ -1,5 +1,32 @@
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+
 from django.shortcuts import render
 
 import requests
 
-from requests import
+
+def index(request):
+    response = requests.get('http://optimal_power_time_calculator:80/api/next-optimal-hour?numHoursToForecast=3h35m')
+    price = json_to_price("Test", response.json()['price'])
+    print(price)
+    return render(request, 'index.html', {'price': price})
+
+
+@dataclass
+class OptimalTimeAppliance:
+    name: str
+    from_dt: datetime
+    to_dt: datetime
+    price: Decimal
+    suboptimal_price_multiplier: Decimal
+
+def json_to_price(name, json_price):
+    return OptimalTimeAppliance(
+        name=name,
+        from_dt=datetime.fromisoformat(json_price['fromTs']),
+        to_dt=datetime.fromisoformat(json_price['toTs']),
+        price=Decimal(json_price['price']),
+        suboptimal_price_multiplier=Decimal(json_price['suboptimalPriceMultiplier']),
+    )
