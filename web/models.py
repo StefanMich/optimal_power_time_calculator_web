dfa80cc52bb448237b771dfe9289c30dda430375
Stefan Micheelsen <sm@intempus.dk>
2025-04-14 22:32:06 +0200

    Add GLNNumber

--- web/models.py
+++ web/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class House(models.Model):
     name = models.CharField(max_length=100)
+    gln_number = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
